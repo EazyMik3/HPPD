89e1741cb7c9f87ee18bc299a406312ac29eabe6
EazyMik3 <86918676+EazyMik3@users.noreply.github.com>
2021-07-05 19:19:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
     introTitle: 'Ayyooooo!',
-    introDesc: `Hum nay là 1 ngày đặc biệt của 1 người`,
+    introDesc: `Gửi đến bạn Thanh Kiều`,
     btnIntro: 'hihi',
-    title: 'Hép pi bớt đê cậu, 20 tuổi mang nhiều niềm vui, ít bị deadline dí😙',
+    title: 'Háp pi bớt đê, 20 tuổi là già thêm 1 tuổi nữa đó, mong là sẽ có nhiều niềm vui và ít bị deadline dí😙',
     desc: 'On the occasion of your 20th birthday, may you forever be young, happy, and beautiful',
-    btnYes: 'Hihi Thanhkew x3000',
+    btnYes: 'Hihi Thankew x3000',
     btnNo: 'Xời xàm xí quá dị😑',
     question:'Thế cho mình xin 10k tiền code này nhé <3',
     btnReply: 'Reply',
     reply: 'Ok luôn ib chuyển khoản liền',
     mess: 'Hê hê uy tín thế là tốt😁',
-    messDesc: 'Khả năng có hạn nên là tới đây thôi😁',
-    btnAccept: 'Okiiiii lun <3',
+    messDesc: 'Coi xong rồi thì giữ một mình nha, đừng đem khoe, tui khổ lắm rồi😥',
+    btnAccept: 'Okiiiii lun',
     messLink: 'https://m.me/blade.hasaki.5'
 }
